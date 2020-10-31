af862130e92e02de6d156fc7e43c7345f9b2da32
Harris shelby <qsobiaolian@gmail.com>
2020-10-31 19:49:37 +0800

    Torqure Balanced finished,happy🤪!

--- abstracts/_variables.js
+++ abstracts/_variables.js
@@ -1,6 +1,18 @@
+const MULT = require('../modules/radomMult');
+
 module.exports = {
     arms: [],
-    innerDiamo: 9.3,
-    low_ratio: 96.5,
-    high_ratio: 98.5
+    numOfLayers: 2,
+    innerDiame: 11.7,
+    maxOutDiame: 17.3,
+    minRatio: 97,
+    maxRatio: 98.5,
+    minBreakingStrength: 86,
+    maxInNum: 30,
+    maxOutNum: 54,
+    minTorqure__ratio: 0.9,
+    maxtorqure__ratio: 1.1,
+    randomMult1: new MULT(),
+    randomMult2: new MULT(),
+    tensile: 0.96
 }
--- index.js
+++ index.js
@@ -1,46 +1,78 @@
 const SPIRAL = require('./modules/spiral');
-const MULT = require('./modules/radomMult');
 
-const randomMult1 = new MULT();
-const randomMult2 = new MULT();
-const arms = [];
+const { arms, numOfLayers, innerDiame, maxOutDiame, 
+    minRatio, maxRatio, minTorqure__ratio, 
+    maxtorqure__ratio, minBreakingStrength, 
+    maxInNum, maxOutNum,
+    randomMult1, randomMult2, tensile} = require('./abstracts/_variables');
 
-const low_ratio = 97;
-const high_ratio = 98.5;
-const low_torqureRate = 0.95;
-const high_torqureRate = 1.05;
-const maxDiame = 17.3
-var cons = false
-var test = -1
-var oddTorqure = -1
-var evenTorqure = -1
-// demo.setValGroup(10, 2, 6)
+let isOk = false;
+let oddEven__ratio = 0
+let breakingStrength = 0
+/**
+ * 
+ * @param {*} innerDiame 
+ * @param {*} numOfLayers 
+ */
+const setSpiralPro = (innerDiame, numOfLayers) => {
+    return new Promise((resolve, reject) => {
+        if(!numOfLayers) reject("Something wrong")
+        let oddTorqure = 0
+        let evenTorqure = 0
+        // let oddEven__ratio = 0
+        arms.push(new SPIRAL(innerDiame, randomMult1.ran, randomMult1.mult))
+        arms.push(new SPIRAL(arms[arms.length-1].outDiame, randomMult2.ran, randomMult2.mult));
+        let oddCon = arms.filter((arm, index) => {
+            return index % 2 !== 0            
+        }).some(el => {
+            return el > maxOutNum
+        })
+
+        let evenCon = arms.filter((arm, index) => {
+            return index % 2 === 0            
+        }).some(el => {
+            return el > maxInNum
+        })
+
+        arms.forEach((arm, index) => {
+            if (index % 2 !== 0 ) {
+                evenTorqure += arm.torque
+            } else {
+                oddTorqure +=arm.torque
+            }
+            breakingStrength += Number(arm.areaOfMono * tensile)
+        })
+
+        oddEven__ratio = oddTorqure / evenTorqure
+        resolve({oddCon, evenCon})
+    })
+}
+
+const judgeSpiralPro = (ProductData) => {
+    return new Promise((resolve, reject) => {
+        if(!arms) reject(err);
+        let isSimilarConditionSatify = arms.every((arm, index) => {
+            return arm.ratio >= minRatio && arm.ratio <= maxRatio
+        })
+
+        let isTorqueBlanced = 
+            oddEven__ratio >= minTorqure__ratio 
+                && oddEven__ratio <= maxtorqure__ratio
+
+        let isOutDiameSatisfy = arms[arms.length-1].outDiame <= maxOutDiame
+
+        let isBreakingStrengthSatisfy = breakingStrength >= minBreakingStrength
+
+        let isnumSatisfy = !(ProductData.oddCon || ProductData.evenCon)
+        resolve(isSimilarConditionSatify && isTorqueBlanced 
+            && isOutDiameSatisfy && isBreakingStrengthSatisfy && isnumSatisfy)
+    })
+}
 
 const setArms = async () => {
     try {
-        oddTorqure = 0
-        evenTorqure = 0
-
-        await arms.push(new SPIRAL(11.7, randomMult1.ran, randomMult1.mult))
-        await arms.push(new SPIRAL(arms[arms.length-1].outDiame, randomMult2.ran, randomMult2.mult));
-        await arms.push(new SPIRAL(arms[arms.length-1].outDiame, randomMult2.ran, randomMult2.mult));
-        cons = await arms.every(arm => {
-            return arm.ratio >= low_ratio && arm.ratio <= high_ratio
-        })
-        arms.filter((arm, index) => {
-            return index % 2 === 0            
-        }).forEach(arm => {
-            oddTorqure += arm.torque 
-        })
-        arms.filter((arm, index) => {
-            return index % 2 !== 0            
-        }).forEach(arm => {
-            evenTorqure += arm.torque
-        })
-        test = arms[0].torque / arms[1].torque
-        randomMult1.init();
-        randomMult2.init();
-        console.log(oddTorqure)
+        let ProductData = await setSpiralPro(innerDiame, numOfLayers);
+        isOk = await judgeSpiralPro(ProductData)
     } catch (err) {
         console.log(err)
         throw err;
@@ -52,19 +84,18 @@ const setArms = async () => {
     try{                
             await setArms()
             while(1) {
-                if (!cons || test<=low_torqureRate || test>=high_torqureRate ) {
-                    // console.log(arms)
+                if (!isOk) {
+                    breakingStrength = 0
+                    randomMult1.init();
+                    randomMult2.init();
                     arms.splice(0, arms.length)
                     await setArms()
-    
                 } else {
                     console.table(arms)
-                    console.log(test)
+                    console.log(oddEven__ratio, breakingStrength)
                     break;
                 }
             }
-
-
     } catch (err) {
         console.log(err)
     }
